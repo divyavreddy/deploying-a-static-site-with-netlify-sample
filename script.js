e907dcd46c29ef588dda940cb985fea990c1087a
divyavreddy <83785927+divyavreddy@users.noreply.github.com>
2021-08-18 15:02:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const ballotButton = document.getElementById('ballot-button');
-const voteSelection = document.getElementById('options');
+const voteSelection1 = document.getElementById('option1');
+const voteSelection2 = document.getElementById('option2');
 
-voteSelection.addEventsListener('click');
+voteSelection1.addEventsListener('click', changeBackground1);
+voteSelection2.addEventsListener('click', changeBackground2);
 ballotButton.addEventListener('click', switchBackground);
 
 function randomColor() {
@@ -12,11 +14,16 @@ function randomColor() {
   const color = `rgb(${red}, ${green}, ${blue})`
   return color;
 }
-/*
-function tally() {
-  const list = voteSelection;
-  document.body
-}*/
+
+function changeBackground1() {
+  const backgroundColor = rgb(240, 255, 255);
+  document.body.style.backgroundColor = backgroundColor;
+}
+
+function changeBackground2() {
+  const backgroundColor = rgb(255, 0, 0);
+  document.body.style.backgroundColor = backgroundColor;
+}
 
 function switchBackground() {
   const backgroundColor = randomColor();
